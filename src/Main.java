0bf0be03ab3a9ed957a2aed6a7b8d22420529d4b
Ann90a <abrosimova222@yandex.ru>
2022-12-07 18:11:36 +0300

    полиндром через конструкцию StringBuffer

--- src/Main.java
+++ src/Main.java
@@ -86,4 +86,16 @@ public class Main {
         }
     }
 
+    public class IsStringPalindrome {
+        public static void main(String[] args) {
+            System.out.println(isPalindrome("шалаш")); // true
+            System.out.println(isPalindrome("ротор")); // true
+            System.out.println(isPalindrome("мотор")); // false
+        }
+
+        public static boolean isPalindrome(String str) {
+            return str.equalsIgnoreCase(new StringBuffer(str).reverse().toString());
+        }
+    }
+
 }
